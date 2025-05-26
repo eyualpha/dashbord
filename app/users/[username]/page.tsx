39ed2ad83941fb76438bb6b41ca876122ba9cb9a
eyualpha <eyuashenafi533@gmail.com>
2025-05-26 19:29:09 +0300

    refactor: update layout title and add hover card component

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -7,6 +7,12 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 
 export default function singleUserPage() {
   return (
@@ -31,7 +37,69 @@ export default function singleUserPage() {
         {/* left side */}
         <div className="flex flex-col w-full xl:w-1/3 space-y-6">
           {/* user badge container */}
-          <div className="bg-primary-foreground p-4 rounded-lg">Badge</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-lg font-medium mb-6">User Badges</h1>
+            <div className="flex gap-4 items-center">
+              <HoverCard>
+                <HoverCardTrigger>
+                  <BadgeCheck
+                    size={36}
+                    className="bg-blue-500/30 rounded-full border-1 border-blue-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This user is verified by the admin.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Shield
+                    size={36}
+                    className="rounded-full bg-green-800/30 border-1 border-green-800/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Admin</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Admin users have access to all features and can manage
+                    users.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Candy
+                    size={36}
+                    className="rounded-full bg-yellow-500/30 border-1 border-yellow-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Awarded</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This user has been awarded for their contributions.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Citrus
+                    size={36}
+                    className="rounded-full bg-orange-500/30 border-1 border-orange-500/50 p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bold mb-2">Popular</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This user has been popular in the community.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+          </div>
           {/* information container */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             informatoin
